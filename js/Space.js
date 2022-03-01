2de5f1fe1a11c78283513f48555a0d25864646a3
aureliefomum <azam_aurelie@yahoo.com>
2022-03-01 20:41:33 -0300

    created mark function for occupied Space

--- js/Space.js
+++ js/Space.js
@@ -22,4 +22,12 @@ class Space {
 
 		document.getElementById("mask").appendChild(svgSpace);
 	}
+
+	/**
+	 * Updates space to reflect a token has been dropped into it.
+	 * @param {Object} token - The dropped token
+	 */
+	mark(token) {
+		this.token = token;
+	}
 }
